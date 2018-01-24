590bb5e4ce7f74078f19cb15487e0d2f57b61615
Zophirel <sworld.design@gmail.com>
2018-01-24 23:49:16 +0100

    ready

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,27 @@
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+var HTMLWebpackPluginConfing = new 
+HTMLWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html'
+  inject: 'body'
+});
+
 module.exports = {
   entry: __dirname + '/app/myapp.js'
   module: {
     loaders: [
       {
-        test: /\.js$
+        test: /\.js$/,
         exclude: /node_modules/
         loader: 'babel-loader'
-      } 
+      }
+     ]
+    },
     output: {
       filename: 'tranformed.js',
-      path __dirname + '/build'
-    }
-   ]
-  }
+      path: __dirname + '/build'
+    },
+    plugins: [HTMLWebpackPluginConfig]
 };
 
 var React = require('react');
