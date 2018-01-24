c6ee9117771fa4feef04d94ffd6bd9eff879e5a1
Zophirel <sworld.design@gmail.com>
2018-01-24 23:26:29 +0100

    transformer

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,20 @@
 module.exports = {
   entry: __dirname + '/app/myapp.js'
+  module: {
+    loaders: [
+      {
+        test: /\.js$
+        exclude: /node_modules/
+        loader: 'babel-loader'
+      } 
+    output: {
+      filename: 'tranformed.js',
+      path __dirname + '/build'
+    }
+   ]
+  }
 };
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 var App = require('./components/App');
